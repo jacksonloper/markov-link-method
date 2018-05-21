27fc3dd64c96cae76c414952946dd615ec6296f4
Jackson Loper <Jackson_Loper@brown.edu>
2018-05-21 14:12:45 -0400

    Used full nxx types for the analysis

--- markovlink/__init__.py
+++ markovlink/__init__.py
@@ -1,10 +1,7 @@
 from . import estimation
 from . import exploration
-from . import criticize
 
 train_q = estimation.train_q
-find_extremal = exploration.find_extremal
-sample_prux = exploration.sample_prux
-sample_rux = exploration.sample_rux
-sample_uniform = exploration.sample_uniform
-train_test_split = criticize.train_test_split
+sample_bootstrap_rux = exploration.sample_bootstrap_rux
+train_test_split = exploration.train_test_split
+get_rux_tildetheta=exploration.get_rux_tildetheta
--- markovlink/exploration.py
+++ markovlink/exploration.py
@@ -6,57 +6,46 @@ import sys
 
 from . import estimation
 
-def sample_prux(plx,qxy,n):
-    '''
-    Consider the polytope of transition matrices q such that plx @ q = plx @ qxy.
+def get_rux_tildetheta(d):
+    q1=np.zeros(d.shape)
+    q1[np.r_[0:d.shape[0]],np.argmax(d,axis=1)]=1
+    
+    q2=np.zeros(d.shape)
+    q2[np.r_[0:d.shape[0]],np.argmax(-d,axis=1)]=1
 
-    Choose a direction uniformly at random, and find the pair of vertices in that polytope
-    which are most extremal along that axis.  Calculate the distance between those vertices
-    projected along the direction, as well as their distance in absolute terms.  
+    return d,q1,q2
 
-    Repeat n times.
+def sample_bootstrap_rux(Nlx,Nly,verbose=True,kappa=.01,use_cvxopt=False,direc=None,bootstrap=True):
 
-    Returns a list of the form
+    if direc is None:
+        direc=npr.randn(Nlx.shape[1],Nly.shape[1])
 
-    [
-        (direction,samp1,samp2,distance_along_axis,absolute_distance),
-        (direction,samp1,samp2,distance_along_axis,absolute_distance),
-        (direction,samp1,samp2,distance_along_axis,absolute_distance),
-    ]
-    '''
+    # do a resample, get extremal
+    if bootstrap:
+        Nlx2=resample(Nlx)
+        Nly2=resample(Nly)
+    else:
+        Nlx2=Nlx
+        Nly2=Nly
+    plx = (Nlx2+1)/np.sum(Nlx2+1,axis=1,keepdims=True)
+    qxy=estimation.train_q(plx,Nly2,kappa=kappa)
+    OPT1=find_extremal(plx,qxy,direc,use_cvxopt=use_cvxopt)
 
-    lst=[]
-    for i in range(n):
-        direc=npr.randn(*qxy.shape)
-        direc=direc/np.linalg.norm(direc)
-        samp1=find_extremal(plx,qxy,direc)
-        samp2=find_extremal(plx,qxy,direc2)
-        lst.append((direc,samp1,samp2,np.sum(direc*(samp1-samp2)),np.linalg.norm(samp1-samp2)))
-    return lst
+    # do another resample, get another extremal
+    if bootstrap:
+        Nlx2=resample(Nlx)
+        Nly2=resample(Nly)
+    else:
+        Nlx2=Nlx
+        Nly2=Nly
+    plx = (Nlx2+1)/np.sum(Nlx2+1,axis=1,keepdims=True)
+    qxy=estimation.train_q(plx,Nly2,kappa=kappa)
+    OPT2=find_extremal(plx,qxy,-direc,use_cvxopt=use_cvxopt)
 
-def sample_rux(plx,qxy,n,verbose=True,update_interval=1):
-    '''
-    Consider the polytope of transition matrices q such that plx @ q = plx @ qxy.
+    return direc,OPT1,OPT2
 
-    Choose a direction uniformly at random, and find vertex in that polytope
-    which is most extremal along that axis.  
 
-    Repeat n times.
-
-    Returns a list of samples.
-    '''
-
-    lst=[]
-    for i in range(n):
-        if verbose and i%update_interval==0:  # ersatz tqdm replacement (didn't want to include tqdm as a requirement)
-            sys.stdout.write('(%d/%d)'%(i,n))
-            sys.stdout.flush()
-        direc=npr.randn(*qxy.shape)
-        samp1=find_extremal(plx,qxy,direc)
-        lst.append(samp1)
-    return np.array(lst)
-
-def find_extremal(plx,qxy,c):
+def find_extremal(plx,qxy,c,use_cvxopt=False):
     '''
     Consider the polytope of transition matrices q such that plx @ q = plx @ qxy.
     Find the most extremal vertex of this polytope in the direction of c.
@@ -71,13 +60,136 @@ def find_extremal(plx,qxy,c):
     # polytope can be defined by A_eq q = b_eq, q>=0:
     A_eq,b_eq = formulate_polytope(plx,qxy)
 
-    # find relevant extremal poitn with interior point method    
-    rez=sp.optimize.linprog(c,A_eq=A_eq,b_eq=b_eq,method='interior-point',options=dict(sym_pos=True,lstsq=True))
-    
-    if rez['success']:
-        return rez['x'].reshape(qxy.shape)
+    # find relevant extremal point with interior point method   
+    if use_cvxopt:
+        return cvxopt_linprog(c,A_eq,b_eq,qxy.ravel()).reshape(qxy.shape)
     else:
-        raise Exception("Linear programming failed!")
+        return scipy_linprog(c,A_eq,b_eq).reshape(qxy.shape)
+
+
+def scipy_linprog(c,A_eq,b_eq):
+    rez=sp.optimize.linprog(c,A_eq=A_eq,b_eq=b_eq,method='interior-point',options=dict(sym_pos=True,lstsq=True))
+    if rez['success']:
+        return rez['x']
+    else:
+        raise Exception("Linear programming failed!")    
+
+def cvxopt_linprog(c,A_eq,b_eq,initialcondition):
+    try:
+        import cvxopt
+    except:
+        raise Exception("Failed to import cvxopt.  Maybe try with use_cvxopt=False?  Scipy is slower though.  Or try conda install cvxopt.")
+
+    G=-np.eye(A_eq.shape[1])
+    g_eq = np.zeros(A_eq.shape[1])
+
+    com=lambda x: cvxopt.matrix(x)
+    rez = cvxopt.solvers.lp(com(c),com(G),com(g_eq),A=com(A_eq),b=com(b_eq),
+                            primalstart=dict(x=com(initialcondition),s=com(initialcondition)),
+                           solver='glpk')
+
+    '''
+    minimize <c, x>
+
+    subj to x  = s
+           Ax  = b
+            s >= 0
+    '''
+
+    
+    if rez['status']=='optimal':
+        return np.array(rez['x'])
+    else:
+        return Exception("Linear programming failed!") 
+
+
+def formulate_polytope(plx,qxy):
+    plx = np.require(plx,dtype=np.float)
+    qxy=np.require(qxy,dtype=np.float)
+
+    assert len(plx.shape)==2,"plx should be a matrix"
+    assert len(qxy.shape)==2,"qxy should be a matrix"
+    d1,d2=plx.shape
+    d2p,d3=qxy.shape
+    assert d2==d2p,'The second dimensions of plx should be the same as the first dimension of qxy'
+    assert estimation.check_transition_matrix(plx),"The rows of plx should sum to 1 and all entries should be positive"
+    assert estimation.check_transition_matrix(qxy),"The rows of qxy should sum to 1 and all entries should be positive"
+
+    d1,d2 = plx.shape
+    d2,d3 = qxy.shape
+
+    # plx @ q = hzy (note we skip the last column of hzy)
+    A_eq1 = np.zeros((d1,d3-1,d2,d3))
+    for z in range(d1):
+        for y in range(d3-1):
+            A_eq1[z,y,:,y] = plx[z,:]
+    A_eq1 = A_eq1.reshape((d1*(d3-1),d2*d3))
+    b_eq1 = (plx @ qxy)[:,:-1].ravel()
+
+    # q is a transition matrix
+    A_eq2 = np.zeros((d2,d2,d3))
+    b_eq2=np.ones(d2)
+    for x in range(d2):
+        A_eq2[x,x]=1
+    A_eq2=A_eq2.reshape((d2,d2*d3))
+    b_eq2=b_eq2.ravel()
+
+    # all eqs
+    A_eq = np.concatenate([A_eq1,A_eq2])
+    b_eq = np.concatenate([b_eq1,b_eq2])
+    
+    return A_eq,b_eq
+
+def train_test_split(Nly,m):
+    '''
+    Input:
+    - an array of positive integers, Nly
+
+    Output:
+    - an array train,test
+
+    For each row, we hold out m of the counts from Nly
+    '''
+
+    assert (np.sum(Nly,axis=1)>=m).all(),"Not all rows of Nly have %d total count; can't hold out that much"%m
+
+    Nly=np.require(Nly)
+    Nshp=Nly.shape
+
+    # sample (l,Y) events, uniformly without replacement
+    # from the (l,Y) events indicated by Nly
+
+    # we iteratively select a random point from Nly
+    # and move it into our new datset
+    Nly=Nly.copy().ravel() # this will hold train
+    Nnew = np.zeros(len(Nly)) # this will hold test
+    for l in range(Nshp[0]):
+        for i in range(m):
+            # choose an event in the lth row of Nly to sample
+            idxs=np.r_[0:Nshp[1]]+Nshp[1]*l
+            idx=npr.choice(idxs,size=1,p=Nly[idxs]/np.sum(Nly[idxs]))[0]
+            Nly[idx]=Nly[idx]-1
+            Nnew[idx]=Nnew[idx]+1
+    Nnew=Nnew.reshape(Nshp)
+
+    return Nly.reshape(Nshp),Nnew.reshape(Nshp)
+
+def resample(tmx):
+    '''
+    Resamples each row of tmx, with replacement
+    '''
+    tmx2=np.zeros(tmx.shape)
+    for z in range(len(tmx)):
+        p=tmx[z]
+        p=p/np.sum(p)
+        tmx2[z]=npr.multinomial(np.sum(tmx[z]),p)
+    return tmx2
+
+
+'''
+uniform sampling stuff
+'''
+
 
 def sample_uniform(plx,qxy,n,dikinshrink=2,verbose=False):
     '''
@@ -207,40 +319,3 @@ def kernel_and_support(p):
     kernel=V[len(e):]
     
     return kernel,support
-
-def formulate_polytope(plx,qxy):
-    plx = np.require(plx,dtype=np.float)
-    qxy=np.require(qxy,dtype=np.float)
-
-    assert len(plx.shape)==2,"plx should be a matrix"
-    assert len(qxy.shape)==2,"qxy should be a matrix"
-    d1,d2=plx.shape
-    d2p,d3=qxy.shape
-    assert d2==d2p,'The second dimensions of plx should be the same as the first dimension of qxy'
-    assert estimation.check_transition_matrix(plx),"The rows of plx should sum to 1 and all entries should be positive"
-    assert estimation.check_transition_matrix(qxy),"The rows of qxy should sum to 1 and all entries should be positive"
-
-    d1,d2 = plx.shape
-    d2,d3 = qxy.shape
-
-    # plx @ q = hzy (note we skip the last column of hzy)
-    A_eq1 = np.zeros((d1,d3-1,d2,d3))
-    for z in range(d1):
-        for y in range(d3-1):
-            A_eq1[z,y,:,y] = plx[z,:]
-    A_eq1 = A_eq1.reshape((d1*(d3-1),d2*d3))
-    b_eq1 = (plx @ qxy)[:,:-1].ravel()
-
-    # q is a transition matrix
-    A_eq2 = np.zeros((d2,d2,d3))
-    b_eq2=np.ones(d2)
-    for x in range(d2):
-        A_eq2[x,x]=1
-    A_eq2=A_eq2.reshape((d2,d2*d3))
-    b_eq2=b_eq2.ravel()
-
-    # all eqs
-    A_eq = np.concatenate([A_eq1,A_eq2])
-    b_eq = np.concatenate([b_eq1,b_eq2])
-    
-    return A_eq,b_eq
